fea9f651b954613f764587bdcd33e28541575ab6
John Carrell <jcarrell@cleversafe.com>
2016-03-06 14:41:03 -0600

    Create the first files for the project
    Created the main.py file, the Heartbeat package and the config file.

---
+++ Heartbeat/Heartbeat.py
@@ -0,0 +1,31 @@
+import ConfigParser
+import threading
+from threading import Thread
+from time import sleep
+
+__author__ = 'john_carrell'
+
+lock = threading.Lock()
+
+class Heartbeat(Thread):
+
+    def __init__(self, idx, delay=1):
+        threading.Thread.__init__(self)
+        if idx is None:
+            raise ValueError("Must provide a value for 'idx' upon Heartbeat instantiation.")
+
+        self._idx = idx
+        self._delay = delay
+        self._running = True
+
+    def run(self):
+        while self._running:
+            lock.acquire()
+            print "THUMP THUMP : {}{}".format(" " * (self._idx * 3), self._idx)
+            lock.release()
+            # threading._sleep(self._delay)
+            sleep(self._delay)
+
+    def stop(self):
+        print "Killing thread: {}".format(self._idx)
+        self._running = False
---
+++ main.py
@@ -0,0 +1,29 @@
+import ConfigParser
+from Heartbeat.Heartbeat import Heartbeat
+import random
+import threading
+
+config = ConfigParser.RawConfigParser()
+config.read("threading_exercise.cfg")
+
+
+def main():
+    max_threads = config.getint("heartbeat", "max_num_threads")
+    threads = []
+    next_idx = 0
+
+    for i in range(max_threads):
+        hb = Heartbeat(next_idx, (random.random() + .1) * 2)
+        hb.start()
+        threads.append(hb)
+        next_idx += 1
+
+    try:
+        while True:
+                pass
+    except KeyboardInterrupt as e:
+        for thread in threads:
+            thread.stop()
+
+if __name__ == "__main__":
+    main()
